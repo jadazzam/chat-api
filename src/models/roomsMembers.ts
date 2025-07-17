84c5afe0d2c111e2b8f3d0d803ecfd787d2a4393
jazzam <jadazzam@me.com>
2025-07-17 15:24:40 -0400

    RoomsMembers model : findByParams method. params : userId & roomId

--- src/models/roomsMembers.ts
+++ src/models/roomsMembers.ts
@@ -4,7 +4,7 @@ import {RoomsMembersType} from "../types/roomsMembers";
 class RoomsMembersModel {
     private pool: PoolDB = new PoolDB();
 
-    async findByParam(param: "user_id" | "room_id", value: unknown) {
+    async findByParam(param: "user_id" | "room_id", value: string) {
         try {
             const query = `SELECT *
                            FROM rooms_members
@@ -16,6 +16,32 @@ class RoomsMembersModel {
             throw e
         }
     }
+
+    async findByParams({userId, roomId}: { userId: string, roomId: string }) {
+        try {
+            let query = 'SELECT * FROM rooms_members';
+            const values = [];
+            const conditions = [];
+            if (userId) {
+                values.push(userId);
+                conditions.push(`user_id = $${values.length}`);
+            }
+            if (roomId) {
+                values.push(roomId);
+                conditions.push(`room_id = $${values.length}`);
+            }
+            console.log("conditions ???", conditions)
+            if (conditions.length) {
+                query += ' WHERE ' + conditions.join(' AND ');
+            }
+            const res = await this.pool.query(query, values);
+            return res.rows as RoomsMembersType[];
+        } catch (e) {
+            console.error('Error RoomsMembersModel findByParams', e);
+            throw e;
+        }
+    }
+
 
     async create(params: { userId: string, roomId: string }) {
         try {
@@ -37,7 +63,8 @@ class RoomsMembersModel {
                 SET active  = $1,
                     left_at = $2
                 WHERE room_id = $3
-                  AND user_id = $4 RETURNING *;
+                  AND user_id = $4
+                  AND left_at IS NULL RETURNING *;
             `;
 
             const now = new Date();
