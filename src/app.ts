85065c95ffded99186ea37191fd7adb2a09feebf
jazzam <jadazzam@me.com>
2025-07-23 15:20:32 -0400

    Socket configuration using httpOnly cookie token

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,33 @@ import express from 'express';
 import usersRouter from './routes/users';
 import authRouter from './routes/auth';
 import messagesRouter from './routes/messages';
-import AuthMiddleware from "./middlewares/auth";
-import roomsMembers from "./routes/roomsMembers";
-import rooms from "./routes/rooms";
-import cors from "cors";
+import AuthMiddleware from './middlewares/auth';
+import roomsMembers from './routes/roomsMembers';
+import rooms from './routes/rooms';
+import cors from 'cors';
 
 const app = express();
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-}))
+  })
+);
+
 app.use(express.json());
 const skipAuthRoutes = ['/signin', '/signup'];
 
 app.use((req, res, next) => {
-    if (skipAuthRoutes.includes(req.path)) {
-        return next();
-    }
-    return AuthMiddleware.verifyToken(req, res, next);
+  if (skipAuthRoutes.includes(req.path)) { 
+    return next();
+  }
+  return AuthMiddleware.verifyToken(req, res, next);
 });
-app.use(authRouter)
+app.use(authRouter);
 app.use('/users', usersRouter);
-app.use('/messages', messagesRouter)
+app.use('/messages', messagesRouter);
 app.use('/rooms', rooms);
-app.use('/rooms-members', roomsMembers)
-export default app;
+app.use('/rooms-members', roomsMembers);
+export default app; 
--- src/middlewares/auth/index.ts
+++ src/middlewares/auth/index.ts
@@ -1,52 +1,57 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import cookie from 'cookie';
+import cookie from "cookie";
 
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET;
 
 class AuthMiddleware {
+  static checkJwtSecret(): boolean {
+    return !!process.env.JWT_SECRET;
+  }
 
-    static checkJwtSecret(): boolean {
-        return !!process.env.JWT_SECRET
+  static verifyToken(req: Request, res: Response, next: NextFunction): void {
+    try {
+      if (!JWT_SECRET) res.status(500).json({ message: "Error JWT token" });
+      const cookies = cookie.parse(req.headers.cookie || "");
+      const token = cookies.auth_token;
+      if (!token) {
+        res.status(401).json({ message: "Not authorized: No token provided" });
+      }
+      (req as any).user = jwt.verify(token || "", JWT_SECRET || "");
+      next();
+    } catch (err) {
+      console.error("Error JWT verification failed:", err);
+      res.status(403).json({ message: "Invalid token", error: err });
     }
+  }
 
+  static decodeToken(token: string) {
+    if (token) token = token.split("auth_token=").pop() ?? "";
+    return token && jwt.verify(token, process.env.JWT_SECRET || "");
+  }
 
-    static verifyToken(req: Request, res: Response, next: NextFunction): void {
-        try {
-            if (!JWT_SECRET) res.status(500).json({message: "Error JWT token"})
-            const cookies = cookie.parse(req.headers.cookie || '');
-            const token = cookies.auth_token;
-            if (!token) {
-                res.status(401).json({message: 'Not authorized: No token provided'});
-            }
-            (req as any).user = jwt.verify(token || '', JWT_SECRET || '');
-            next();
-        } catch (err) {
-            console.error("Error JWT verification failed:", err);
-            res.status(403).json({message: 'Invalid token', error: err});
-        }
+  static async createToken(payload: {
+    email: string;
+    name: string;
+    id: string;
+  }): Promise<string> {
+    const { email, name, id } = payload;
+    if (!this.checkJwtSecret()) throw new Error("Error signing in");
+    try {
+      return jwt.sign(
+        {
+          email,
+          name,
+          id
+        },
+        process.env.JWT_SECRET || "",
+        { expiresIn: "7d" }
+      );
+    } catch (err) {
+      console.error("Error JWT create token", err);
+      throw err;
     }
-
-    static decodeToken(token: string | undefined) {
-        if (token?.startsWith('Bearer ')) token = token.split("Bearer ").pop()
-        return token && jwt.verify(token, process.env.JWT_SECRET || '');
-    }
-
-    static async createToken(payload: { email: string; name: string, id: string }): Promise<string> {
-        const {email, name, id} = payload
-        if (!this.checkJwtSecret()) throw new Error('Error signing in');
-        try {
-            return jwt.sign({
-                email,
-                name,
-                id
-            }, process.env.JWT_SECRET || '', {expiresIn: '7d'})
-        } catch (err) {
-            console.error("Error JWT create token", err)
-            throw err
-        }
-
-    }
+  }
 }
 
 export default AuthMiddleware;
